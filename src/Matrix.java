f5a82cc4b0e20c4261095266bc3751c7a1d8e9d1
Zach Thornton <thorntonz@wit.edu>
2014-07-16 14:07:54 -0400

    Added additional matrix class functionality

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,11 +1,53 @@
 
 public class Matrix {
-	int[][] matrix;
+	
+	private int[][] matrix;
 	
 	public Matrix(){
+		this.matrix = new int[Bankers.MAX_PROCESSES][Bankers.MAX_RESOURCES];
+	}
+	
+	public Matrix(int x, int y){
+		this.matrix = new int[x][y];
+	}
+	
+	//J will represent the appropriate row to be summed
+	public int getRowSum(int j){
+		int val = 0;
 		
+		for(int i=0; i<Bankers.MAX_RESOURCES; i++){
+				val+=matrix[i][j];
+		}
+		return val;
 	}
 	
 	public int[][] getMatrix(){ return this.matrix;}
-
+	
+	public int[] getRow(int index){
+		int[] row = new int[Bankers.MAX_PROCESSES];
+		for(int i=0; i< Bankers.MAX_PROCESSES; i++){
+			row[i] = matrix[i][index];
+		}
+		return row;
+	}
+	
+	public int[] getCol(int index){
+		int col[] = new int[Bankers.MAX_RESOURCES];
+		
+		for(int i=0; i<Bankers.MAX_RESOURCES; i++){
+			col[i] = matrix[index][i];
+		}
+		return col;
+	}
+	
+	//Copy fxn
+	public int[][] Copy(){
+		int[][] temp = new int[Bankers.MAX_RESOURCES][Bankers.MAX_PROCESSES];
+		for(int i=0; i < Bankers.MAX_PROCESSES; i++){
+			for(int j=0;j<Bankers.MAX_RESOURCES; j++){
+				temp[j][i] = this.matrix[j][i];
+			}
+		}
+		return temp;
+	}
 }
